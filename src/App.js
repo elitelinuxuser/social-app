3755b22ebe0210e92db62603ad47b707848cf7cd
Lucifer <ramanandsirvi21@gmail.com>
2018-07-30 13:42:06 +0530

    Webpack setup for scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
