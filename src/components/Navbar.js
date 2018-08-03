20db521096db0d7213b27ba1197b3934e4c8ecdd
Lucifer <ramanandsirvi21@gmail.com>
2018-08-03 20:08:22 +0530

    Navbar fixed to top instead of being sticky

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,6 @@ import {
   DropdownMenu,
   DropdownItem
 } from "mdbreact";
-import { BrowserRouter as Router } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../reducers/ActionCreators";
 
@@ -57,62 +56,60 @@ class NavbarHeader extends React.Component {
 
   render() {
     return (
-      <Router>
-        <Navbar color="indigo" dark expand="md" sticky="top" scrolling>
-          <NavbarBrand href="/">
-            <strong>Navbar</strong>
-          </NavbarBrand>
-          {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
-          <Collapse isOpen={this.state.collapse} navbar>
-            <NavbarNav left>
-              <NavItem active>
-                <NavLink to="#">Home</NavLink>
-              </NavItem>
+      <Navbar color="indigo" dark expand="md" fixed="top" scrolling>
+        <NavbarBrand href="/">
+          <strong>Navbar</strong>
+        </NavbarBrand>
+        {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
+        <Collapse isOpen={this.state.collapse} navbar>
+          <NavbarNav left>
+            <NavItem active>
+              <NavLink to="#">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="#">Friends</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="#">Colleges</NavLink>
+            </NavItem>
+            <NavItem className="nav-search">
+              <form className="form-inline">
+                <input
+                  className="form-control form-control-sm mr-sm-2"
+                  type="text"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={this.state.userName}
+                  onChange={this.handleSearch}
+                />
+              </form>
+            </NavItem>
+          </NavbarNav>
+          {this.props.user.email && (
+            <NavbarNav right>
               <NavItem>
-                <NavLink to="#">Friends</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="#">Colleges</NavLink>
-              </NavItem>
-              <NavItem className="nav-search">
-                <form className="form-inline">
-                  <input
-                    className="form-control form-control-sm mr-sm-2"
-                    type="text"
-                    placeholder="Search"
-                    aria-label="Search"
-                    value={this.state.userName}
-                    onChange={this.handleSearch}
-                  />
-                </form>
+                <Dropdown
+                  size="lg"
+                  isOpen={this.props.dropdownOpen}
+                  toggle={this.props.toggle}
+                >
+                  <DropdownToggle nav>
+                    <i className="fa fa-user icon-profile" />
+                    <i className="fa fa-angle-down" />
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem href="#">Profile</DropdownItem>
+                    <DropdownItem href="#">Settings</DropdownItem>
+                    <DropdownItem href="#" onClick={this.handleLogout}>
+                      Logout
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
               </NavItem>
             </NavbarNav>
-            {this.props.user.email && (
-              <NavbarNav right>
-                <NavItem>
-                  <Dropdown
-                    size="lg"
-                    isOpen={this.props.dropdownOpen}
-                    toggle={this.props.toggle}
-                  >
-                    <DropdownToggle nav>
-                      <i className="fa fa-user icon-profile" />
-                      <i className="fa fa-angle-down" />
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem href="#">Profile</DropdownItem>
-                      <DropdownItem href="#">Settings</DropdownItem>
-                      <DropdownItem href="#" onClick={this.handleLogout}>
-                        Logout
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </Dropdown>
-                </NavItem>
-              </NavbarNav>
-            )}
-          </Collapse>
-        </Navbar>
-      </Router>
+          )}
+        </Collapse>
+      </Navbar>
     );
   }
 }
